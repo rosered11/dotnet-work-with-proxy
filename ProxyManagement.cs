62efbd1b260ccd889450b8dff58d77061049c9bf
Rosered <krihsana5@gmail.com>
2021-07-10 17:46:59 +0700

    fix bug

--- ProxyManagement.cs
+++ ProxyManagement.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Client.With.Proxy.Demo
 {
