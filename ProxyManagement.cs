a56ed0bf68d1752c5ac826c74dea1df071cff7a0
Rosered <krihsana5@gmail.com>
2021-07-10 17:55:16 +0700

    add condition for forward

--- ProxyManagement.cs
+++ ProxyManagement.cs
@@ -6,12 +6,17 @@ namespace Client.With.Proxy.Demo
 {
     public static class ProxyManagement
     {
-        public static List<string> ignore = new List<string> { "" };
+        public static List<string> ignore = new List<string> { "/api/A" };
         public static void ConfigWithProxy(this IApplicationBuilder app, IConfiguration configuration)
         {
             // First pipeline
             app.Use(async (context, next) => 
             {
+                if (ignore.Contains(context.Request.Path.Value))
+                {
+                    context.Request.Headers.Remove("X-Forwarded-For");
+                    context.Request.Headers.Remove("X-Forwarded-Proto");
+                }
                 //await context.Response.WriteAsync("First pipeline step 1");
                 await next();
                 //await context.Response.WriteAsync("First pipeline step 2");
